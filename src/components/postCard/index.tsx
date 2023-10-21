590dd3e13a473f6713947a85c70268cb56d40fe1
lxy-Jason <1627962483@qq.com>
2023-10-21 21:59:03 +0800

    style: 样式完善

--- src/components/postCard/index.tsx
+++ src/components/postCard/index.tsx
@@ -1,12 +1,11 @@
-'use client'
+'use client';
 
-import TopPinIcon from "./components/topPinIcon";
-import {Title} from './components/title'
-import Link from "next/link";
-import {getTarget} from "@/utils/getTarget";
-import Markdown from "@/components/markdown";
-import {useMemo, useState} from "react";
-
+import TopPinIcon from './components/topPinIcon';
+import { Title } from './components/title';
+import Link from 'next/link';
+import { getTarget } from '@/utils/getTarget';
+import Markdown from '@/components/markdown';
+import { useMemo, useState } from 'react';
 
 export default function (props: {
   id: number | string;
@@ -15,15 +14,15 @@ export default function (props: {
   createdAt: Date;
   category: string;
   content: string;
-  type: "overview" | "article" | "about";
-  path: string,
+  type: 'overview' | 'article' | 'about';
+  path: string;
   pay?: string[];
   payDark?: string[];
   author?: string;
   tags?: string[];
   next?: { id: number; title: string; pathname?: string };
   pre?: { id: number; title: string; pathname?: string };
-  enableComment: "true" | "false";
+  enableComment: 'true' | 'false';
   top?: number;
   private?: boolean;
   // showDonateInAbout?: boolean;
@@ -35,53 +34,60 @@ export default function (props: {
   // showExpirationReminder: boolean;
   showEditButton: boolean;
 }) {
-  const {content} = props;
-  const [lock, setLock] = useState(props.type != "overview" && props.private);
+  const { content } = props;
+  const [lock, setLock] = useState(props.type != 'overview' && props.private);
 
   const calContent = useMemo(() => {
-    if (props.type == "overview") {
+    if (props.type == 'overview') {
       if (props.private) {
-        return "该文章已加密，点击 `阅读全文` 并输入密码后方可查看。";
+        return '该文章已加密，点击 `阅读全文` 并输入密码后方可查看。';
       }
-      const r = content.split("<!-- more -->");
+      const r = content.split('<!-- more -->');
       if (r.length > 1) {
         return r[0];
       } else {
-        return content.substring(0, 100);
+        return content;
       }
     } else {
-      return content.replace("<!-- more -->", "");
+      return content.replace('<!-- more -->', '');
     }
   }, [props, lock, content]);
 
   return (
-    <div
-      className={'post-card-wrapper mb-5'}
-    >
+    <div className={'post-card-wrapper mb-5'}>
       <div
-        style={{position: "relative"}}
-        id="post-card"
-        className="overflow-hidden post-card bg-white card-shadow py-4 px-1 sm:px-3 md:py-6 md:px-5 dark:bg-dark  dark:nav-shadow-dark"
-      >
+        style={{ position: 'relative' }}
+        id='post-card'
+        className='post-card card-shadow dark:bg-dark dark:nav-shadow-dark overflow-hidden bg-white px-1 py-4 sm:px-3 md:px-5  md:py-6'>
         {props.top != 0 && <TopPinIcon></TopPinIcon>}
         <Title
           type={props.type}
           id={props.id}
           title={props.title}
           openArticleLinksInNewWindow={props.openArticleLinksInNewWindow}
-          showEditButton={props.showEditButton}
-        ></Title>
+          showEditButton={props.showEditButton}></Title>
+
         {/*  子标题todo*/}
-        <div className="text-sm md:text-base  text-gray-600 mt-4 mx-2">
-          <Markdown content={calContent}></Markdown>
-          {props.type == "overview" && (
-            <div className="w-full flex justify-center mt-4 ">
+        <div className='relative mx-2  mt-4 text-sm text-gray-600 md:text-base'>
+          <div
+            className='overflow-hidden'
+            style={{
+              minHeight: '300px',
+              maxHeight: props.type === 'overview' ? '500px' : '',
+            }}>
+            <Markdown content={calContent}></Markdown>
+          </div>
+          {props.type === 'overview' && (
+            <div className='absolute bottom-0 z-10 flex w-full justify-center bg-gradient-to-b from-transparent to-white pb-2.5 pt-16 dark:to-black'>
               <Link
                 href={`/post/${props.id}`}
-                target={getTarget(props.openArticleLinksInNewWindow)}
-              >
+                target={getTarget(props.openArticleLinksInNewWindow)}>
                 <div
-                  className=" dark:bg-dark dark:hover:bg-dark-light dark:hover:text-dark-r dark:border-dark dark:text-dark hover:bg-gray-800 hover:text-gray-50 border-2 border-gray-800 text-sm md:text-base text-gray-700 px-2 py-1 transition-all rounded">
+                  className=' dark:bg-dark dark:hover:bg-dark-light dark:hover:text-dark-r
+                    dark:border-dark dark:text-dark bg rounded border-2 border-gray-800 px-2
+                    py-1 text-sm text-gray-700 transition-all hover:bg-gray-800 hover:text-gray-50
+                    md:text-base
+                     '>
                   阅读全文
                 </div>
               </Link>
@@ -90,5 +96,5 @@ export default function (props: {
         </div>
       </div>
     </div>
-  )
+  );
 }
